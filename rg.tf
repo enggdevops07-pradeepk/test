4ddeb348243e512efc3d93560947dd27227e1948
Your Name <enggdevops07@gmail.com>
2026-06-03 15:38:58 +0530

    new-push

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
   name = "test-rg"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name = "test-rg1"
+  location = "centralindia"
+}
