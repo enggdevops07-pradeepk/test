6688c7493f8a886dfbdc75e08b64b11728664896
Your Name <enggdevops07@gmail.com>
2026-06-03 15:45:04 +0530

    3rd

--- rg.tf
+++ rg.tf
@@ -7,3 +7,13 @@ resource "azurerm_resource_group" "rg2" {
   name = "test-rg1"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "rg1" {
+  name = "test-rg"
+  location = "centralindia"
+}
+
+resource "azurerm_resource_group" "rg3" {
+  name = "test-rg3"
+  location = "centralindia"
+}
